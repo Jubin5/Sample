c9217d2dec897412da3a2876c056f015c8258e4e
Jubin K Babu <jubinkbabu5@gmail.com>
2025-07-21 11:03:46 +0530

    Update bigdatastreamlit.py

--- bigdatastreamlit.py
+++ bigdatastreamlit.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1hIQJZYfK22mKB2_bO2cVrolxSgt8FaIh
 """
 
-pip install streamlit
 
 import streamlit as st
 import pandas as pd
